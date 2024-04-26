eefbf027133b043d68ac7a82e6f86d54f38ea83e
LuciaLacort <lacortlucia@gmail.com>
2024-04-26 21:29:36 +0200

    filter and warning

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,1 +1,100 @@
 console.log('functions ready');
+
+//En esta página voy  a hacer solo las funcionalidades de JS 
+
+//1º Traer del html lo que necesite, en este caso me tengo que fijar en las clases que he puesto con set atribute
+
+const searchBtn = document.querySelector('.js-search-btn');
+const searchField = document.querySelector('.js-search-input');
+const resetBtn = document.querySelector('.js-reset-btn');
+const drinksList = document.querySelector('.js-drinks-list');
+const warning = document.querySelector('.js-search-warning');
+
+
+
+
+//Declaro dos variables: la que será el array de bebidas y la que será el array de las bebidas fav
+
+let drinks = [];
+let favDrinks = [];
+
+//Tengo que poner la función de añadir favorito antes porque luego la tengo que llamar para el click encima de cada li
+
+const addFavDrink = (event) => {
+    console.log(event.currentTarget.id);
+    const clickedDrink = event.currentTarget.id;
+    const drink = drinks.find((drink) => drink.idDrink === drinkId);
+    favDrinks.push(drink);
+}
+
+
+//Saco los datos por pantalla con una función render (hago una que tenga el li dentro del bucle para no hacer dos funciones):
+
+
+const renderDrinksList = (array) => {
+    drinksList.innerHTML = '';
+    for (const drink of array){
+        const drinkName = drink.strDrink;
+        const drinkImg = drink.strDrinkThumb;
+        const drinkId = drink.idDrink;
+        drinksList.innerHTML += `
+        <li class="drinks__section--item js-drink" id="${drinkId}">
+            <img class="drink__img" src="${drinkImg}" alt="${drinkName}">
+            <h3 class="drinks__name">${drinkName}</h3>
+        </li>
+        `;
+    }
+}
+
+
+
+//Traigo los datos de la api y ya los dejo guardados en local storage
+
+const getApiData = () => {
+
+    const searchWord = searchField.value;
+    fetch (
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchWord}`
+
+    )
+    .then(response => response.json())
+    .then((dataApi) => {
+        drinks = dataApi.drinks;
+        console.log(drinks);
+        renderDrinksList(drinks);
+        localStorage.setItem('drinks', JSON.stringify(drinks));
+    })
+};
+
+const handleSearch = (event) => {
+    event.preventDefault();
+    const searchValue = searchField.value;
+    if(searchValue === ''){
+        warning.classList.remove('hidden');
+        warning.innerHTML = 'Ups, parece que no has elegido un cocktail.<br>Introduce en el buscador el nombre de tu cocktail favorito.';
+    } else {
+        warning.classList.add('hidden');
+        const searchedDrinks = drinks.filter((drink) => 
+        drink.strDrink.toLowerCase().includes(searchValue.toLowerCase()));
+        renderDrinksList(searchedDrinks);
+        console.log(searchedDrinks);
+        getApiData();
+    }
+}
+
+const handleReset = (event) => {
+    event.preventDefault();
+}
+
+const init = () => {
+    drinks = JSON.parse(localStorage.getItem('drinks'));
+    renderDrinksList(drinks);
+}
+
+//Aqui va lo que se tiene que ejecutar cuando el usuario abra la página (algo que me pinte solo los margarita y los botones)
+
+init();
+searchBtn.addEventListener('click', handleSearch);
+resetBtn.addEventListener('click', handleReset);
+
+
--- src/js/page.js
+++ src/js/page.js
@@ -1,11 +1,21 @@
 console.log('pages ready');
 
+//Tengo que crear el html desde aquí, entonces primero voy a decir las etiquetas que quiero crear:
+
 const mainPage = document.querySelector('.js-page');
 const mainTitle = document.createElement('h1');
 const form = document.createElement('form');
 const searchInput = document.createElement('input');
 const searchButton = document.createElement('button');
 const resetButton = document.createElement('button');
+const section = document.createElement('section');
+const createFavUl = document.createElement('ul');
+const createUl = document.createElement('ul');
+const searchWarning = document.createElement('p');
+// const createLi = document.createElement('li');
+
+
+//Despues, una por una, tengo que ir metiendo los atributos que normalmente les pondría en la etiqueta de apertura. Después, tengo que ponerle contenido (si tienen) y decir quien es su madre para que se coloquen dentro:
 
 
 mainTitle.setAttribute('class', 'main__page--title');
@@ -19,16 +29,51 @@ searchInput.setAttribute('type', 'text');
 searchInput.setAttribute('id', 'search');
 searchInput.setAttribute('placeholder', 'Introduce el nombre del cocktail');
 searchInput.setAttribute('name', 'search');
-searchInput.setAttribute('class', 'main__page--search');
+searchInput.setAttribute('class', 'js-search-input');
+searchInput.classList.add('main__page--search');
 form.appendChild(searchInput);
 
 searchButton.setAttribute('id', 'searchBtn');
+searchButton.setAttribute('class', 'js-search-btn');
+searchButton.classList.add('search__button');
 searchButton.textContent = 'Buscar';
 form.appendChild(searchButton);
 
-resetButton.setAttribute('id', 'searchBtn');
+resetButton.setAttribute('id', 'resetBtn');
+resetButton.setAttribute('class', 'js-reset-btn');
+resetButton.classList.add('reset__button');
 resetButton.textContent = 'Reset';
 form.appendChild(resetButton);
+
+searchWarning.setAttribute('class', 'js-search-warning');
+searchWarning.classList.add('search__warning');
+searchWarning.classList.add('hidden');
+
+mainPage.appendChild(searchWarning);
+
+
+section.setAttribute('class', 'js-section');
+section.classList.add('drinks__section');
+mainPage.appendChild(section);
+
+createFavUl.setAttribute('class', 'js-fav-drinks-list');
+createFavUl.classList.add('drinks__section--list');
+section.appendChild(createFavUl);
+
+createUl.setAttribute('class', 'js-drinks-list');
+createUl.classList.add('drinks__section--list');
+section.appendChild(createUl);
+
+
+
+// createLi.setAttribute('class', 'drinks__item');
+// createLi.setAttribute('class', 'js-drink');
+// createUl.appendChild(createLi);
+
+
+
+
+
 
 
 
